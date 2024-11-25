16f5bf009bc7edd9de0e09de721c7f2aec9195ba
julien <julien.mrss@gmail.com>
2024-11-25 14:33:43 +0100

    add home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,14 +6,26 @@ import {
   IonTitle,
   IonContent,
   IonButton,
-} from '@ionic/angular/standalone';
+  IonCardTitle,
+  IonCardHeader,
+  IonCard, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule],
+  imports: [IonCol, IonRow, IonGrid, 
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    RouterModule,
+  ],
 })
 export class HomePage {
   constructor() {}
