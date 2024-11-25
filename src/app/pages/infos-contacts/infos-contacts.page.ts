e577bafcbc4be69dd3ed8214c24ff07fc211703f
julien <julien.mrss@gmail.com>
2024-11-25 14:34:08 +0100

    add infos contacts

--- src/app/pages/infos-contacts/infos-contacts.page.ts
+++ src/app/pages/infos-contacts/infos-contacts.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoFacebook, logoYoutube, logoInstagram } from 'ionicons/icons';
 
 @Component({
   selector: 'app-infos-contacts',
   templateUrl: './infos-contacts.page.html',
   styleUrls: ['./infos-contacts.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonIcon,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class InfosContactsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ logoFacebook, logoYoutube, logoInstagram });
   }
 
+  ngOnInit() {}
 }
