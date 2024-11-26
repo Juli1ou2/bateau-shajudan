59a76b69bde8987612e9ab3ef409198e15d78db9
julien <julien.mrss@gmail.com>
2024-11-26 09:31:36 +0100

    add recette infos page

--- src/app/pages/recettes/infos-recette/infos-recette.page.ts
+++ src/app/pages/recettes/infos-recette/infos-recette.page.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardTitle,
+  IonCardContent,
+  IonCardHeader,
+} from '@ionic/angular/standalone';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Recette } from 'src/app/interface/recette';
 
 @Component({
   selector: 'app-infos-recette',
   templateUrl: './infos-recette.page.html',
   styleUrls: ['./infos-recette.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCardHeader,
+    IonCardContent,
+    IonCardTitle,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class InfosRecettePage implements OnInit {
+  recette: Recette;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.recette =
+          this.router.getCurrentNavigation()?.extras.state?.['recette'];
+      }
+    });
   }
-
 }
