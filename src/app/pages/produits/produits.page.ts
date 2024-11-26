770f5ffdecd7fbd1f3e2b00772582b6bcc3e5d62
julien <julien.mrss@gmail.com>
2024-11-26 17:19:05 +0100

    add header component (back button) to produits.page

--- src/app/pages/produits/produits.page.ts
+++ src/app/pages/produits/produits.page.ts
@@ -1,48 +1,49 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {ProduitService} from "../../services/produit.service";
-import {Produit} from "../../core/interface/produit";
-import {IonicModule} from "@ionic/angular";
+import { ProduitService } from '../../services/produit.service';
+import { Produit } from '../../core/interface/produit';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from 'src/app/ui/header/header.component';
 
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.page.html',
   styleUrls: ['./produits.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule]
+  imports: [CommonModule, FormsModule, IonicModule, HeaderComponent],
 })
 export class ProduitsPage implements OnInit {
-
+  title: string = 'Produits';
   produitService: ProduitService = inject(ProduitService);
   produitList: Produit[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.getProducts()
+    this.getProducts();
   }
 
   getProducts() {
     this.produitService.getProducts().subscribe({
-      next: (products)=> {
-        this.produitList = products
+      next: (products) => {
+        this.produitList = products;
         this.produitList.sort((a, b) => a.name.localeCompare(b.name));
 
-        this.calculNewPrice()
+        this.calculNewPrice();
       },
-      error: err => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
   }
 
   calculNewPrice() {
     for (let produit of this.produitList) {
       if (produit.discount && produit.discount > 0) {
-        produit['newPrice'] = produit.price - (produit.price * produit.discount / 100);
+        produit['newPrice'] =
+          produit.price - (produit.price * produit.discount) / 100;
       } else {
         produit['newPrice'] = produit.price;
       }
     }
   }
-
 }
