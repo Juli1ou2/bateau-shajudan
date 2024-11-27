14e646447a3f9380a6c58dd7972caec98c214e4e
Juli1ou2 <julien.mrss@gmail.com>
2024-11-27 19:49:40 +0100

    add title to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,38 +17,47 @@ export const routes: Routes = [
       },
       {
         path: 'infos-contacts',
+        title: 'Contacts | Shajudan Boat',
         loadComponent: () => import('./pages/infos-contacts/infos-contacts.page').then( m => m.InfosContactsPage)
       },
       {
         path: 'bateaux',
+        title: 'Bateaux | Shajudan Boat',
         loadComponent: () => import('./pages/bateaux/bateaux.page').then( m => m.BateauxPage)
       },
       {
         path: 'restaurants',
+        title: 'Restaurants | Shajudan Boat',
         loadComponent: () => import('./pages/restaurants/restaurants.page').then( m => m.RestaurantsPage)
       },
       {
         path: 'produits',
+        title: 'Produits | Shajudan Boat',
         loadComponent: () => import('./pages/produits/produits.page').then( m => m.ProduitsPage)
       },
       {
         path: 'recettes',
+        title: 'Recettes | Shajudan Boat',
         loadComponent: () => import('./pages/recettes/recettes.page').then( m => m.RecettesPage)
       },
       {
         path: 'panier',
+        title: 'Panier | Shajudan Boat',
         loadComponent: () => import('./pages/panier/panier.page').then( m => m.PanierPage)
       },
       {
         path: 'infos-bateau',
+        title: 'Détails bateau | Shajudan Boat',
         loadComponent: () => import('./pages/bateaux/infos-bateau/infos-bateau.page').then( m => m.InfosBateauPage)
       },
       {
         path: 'infos-recette',
+        title: 'Détails recette | Shajudan Boat',
         loadComponent: () => import('./pages/recettes/infos-recette/infos-recette.page').then( m => m.InfosRecettePage)
       },
       {
         path: 'infos-restaurant',
+        title: 'Détails restaurant | Shajudan Boat',
         loadComponent: () => import('./pages/restaurants/infos-restaurant/infos-restaurant.page').then( m => m.InfosRestaurantPage)
       },
     ]
