af0886c1264973f27000623c820489df1d5efc09
julien <julien.mrss@gmail.com>
2024-11-29 13:33:39 +0100

    change appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'bateau-shajudan',
+  appName: 'Bateau Shajudan',
   webDir: 'www'
 };
 
